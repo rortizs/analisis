102ac9937d715137946bcabe59137603e00ea5eb
Richard Ortiz <37150696+rortizs@users.noreply.github.com>
2021-05-21 13:49:44 -0600

    add code firs
    code

---
+++ analisis.Datos/DbContextSistema.cs
@@ -0,0 +1,24 @@
+﻿using analisis.Datos.Mapping.Almacen;
+using analisis.Entidades.Almacen;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace analisis.Datos
+{
+    public class DbContextSistema : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbContextSistema(DbContextOptions<DbContextSistema>options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+        }
+    }
+}
