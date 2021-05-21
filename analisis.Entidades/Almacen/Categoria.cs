605389620237b319436eb701800a7a41ce87dc98
Richard Ortiz <37150696+rortizs@users.noreply.github.com>
2021-05-21 13:50:22 -0600

    add entity
    add

---
+++ analisis.Entidades/Almacen/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace analisis.Entidades.Almacen
+{
+   public class Categoria
+    {
+        public int idcategoria { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength =3, ErrorMessage ="El nombre debe tener al menos 3 caracteres o un maximo de 50")]
+        public string nombre { get; set; }
+
+        [StringLength(256)]
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+    }
+}
